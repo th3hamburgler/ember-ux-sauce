383acfb5b4c24ba635e5feec5f02f9abf0d87d8e
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-01-02 21:43:39 +0000

    Docs (forms) Added form doc routes

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -8,8 +8,15 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('docs', function() {
-    this.route('installation');
     this.route('quickstart');
+    this.route('forms', function() {
+      this.route('buttons');
+      this.route('input-controls');
+      this.route('dropdown-controls');
+      this.route('toggles');
+      this.route('validation');
+      this.route('fieldsets');
+    });
     this.route('api', function() {
       this.route('class', {
         path: '/:class_id'
