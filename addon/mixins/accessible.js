4229faac6e673cc7574e2563813af6ed7bfe9a52
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-01-04 11:01:19 +0000

    Docs (accessibility) Add jsdocs

--- addon/mixins/accessible.js
+++ addon/mixins/accessible.js
@@ -4,10 +4,32 @@ const {
   Mixin,
 } = Ember;
 
+/**
+  A mixin to add accesibility attributes to a component
+  @class Accessible
+  @namespace Mixins
+  @public
+*/
 export default Mixin.create({
   attributeBindings: [
     'disabled:aria-disabled',
     'label:aria-label',
     'role',
   ],
+  /**
+    Sets the an aria-label attribute
+    @property label
+    @default  null
+    @type     {(string)}
+    @public
+   */
+  label: false,
+  /**
+    Sets the role attribute
+    @property role
+    @default  null
+    @type     {(string)}
+    @public
+   */
+  role: false,
 });
