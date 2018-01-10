f0c13d675fb88f6c7b6ee63e9cc65bc4580b85f3
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-01-10 22:10:57 +0000

    Feat (forms) Added form error component

---
+++ addon/components/uxs-form-error.js
@@ -0,0 +1,28 @@
+import PropTypeMixin, {
+  PropTypes
+} from 'ember-prop-types';
+import Component from '@ember/component';
+import Testable from 'ember-ux-sauce/mixins/testable';
+import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
+import layout from '../templates/components/uxs-form-error';
+
+const FormError = Component.extend(BEMComponent, PropTypeMixin, Testable, {
+  // Attributes
+  base: 'uxs-form__error',
+  layout,
+  // Methods
+  init() {
+    this.set('modifiers', ['disabled']);
+    this._super(...arguments);
+    this.set('propTypes', {
+      text: PropTypes.string,
+      disabled: PropTypes.boolean,
+    });
+  },
+});
+
+FormError.reopenClass({
+  positionalParams: ['text']
+});
+
+export default FormError;
---
+++ tests/integration/components/uxs-form-error-test.js
@@ -0,0 +1,34 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('uxs-form-error', 'Integration | Component | uxs form error', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.set('error', 'Oooh, that\'s not good');
+
+  this.render(hbs `{{uxs-form-error error name="my-error"}}`);
+
+  let $error = this.$('[data-test-uxs-form__error="my-error"]')
+
+  assert.equal($error.prop("tagName"), 'DIV', 'Has correct tag name');
+  assert.equal($error.text().trim(), 'Oooh, that\'s not good', 'Has correct text');
+  assert.equal($error.attr("class"), 'uxs-form__error ember-view', true, 'Has correct classes');
+});
+
+test('it renders a disabled error', function(assert) {
+  this.set('error', 'Oooh, that\'s not good');
+  this.set('disabled', true);
+
+  this.render(hbs `{{uxs-form-error error name="my-error" disabled=disabled}}`);
+
+  let $error = this.$('[data-test-uxs-form__error="my-error"]')
+
+  assert.equal($error.prop("tagName"), 'DIV', 'Has correct tag name');
+  assert.equal($error.text().trim(), 'Oooh, that\'s not good', 'Has correct text');
+  assert.equal($error.attr("class"), 'uxs-form__error uxs-form__error--disabled ember-view', true, 'Has correct classes');
+});
