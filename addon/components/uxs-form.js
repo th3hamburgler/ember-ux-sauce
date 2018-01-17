fb2ee231fba041cab2966c24de5e0b341574bbbe
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-01-17 23:58:13 +0000

    Feat (forms) Add submit contextual button definition

--- addon/components/uxs-form.js
+++ addon/components/uxs-form.js
@@ -1,6 +1,21 @@
 import Component from '@ember/component';
 import layout from '../templates/components/uxs-form';
+import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
 
-export default Component.extend({
-  layout
+export default Component.extend(BEMComponent, {
+  // Attributes
+  attributeBindings: ['novalidate'],
+  base: 'uxs-form',
+  layout,
+  novalidate: true,
+  tagName: 'form',
+  // Actions
+  actions: {
+    /**
+     * Default form submit handler
+     **/
+    submit() {
+      this.submit();
+    }
+  }
 });
--- tests/dummy/app/controllers/docs/forms/index.js
+++ tests/dummy/app/controllers/docs/forms/index.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import {
+  get,
   set
 } from '@ember/object';
 export default Controller.extend({
@@ -7,5 +8,13 @@ export default Controller.extend({
     this._super(...arguments);
     set(this, 'genders', ['Female', 'Male', 'Unspecified']);
   },
-
+  // Actions
+  // BEGIN-SNIPPET form-demo
+  actions: {
+    save(model) {
+      window.alert(`Save model ${get(model, 'firstName')}`);
+      return false;
+    }
+  },
+  // END-SNIPPET
 });
