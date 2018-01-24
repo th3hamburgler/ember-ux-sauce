ab21a44895a925db4f2514863320febe3b9f35f0
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-01-24 21:22:17 +0000

    Feat (nav) Make nav items clickable

--- addon/components/uxs-nav-item.js
+++ addon/components/uxs-nav-item.js
@@ -1,9 +1,10 @@
 import Component from '@ember/component';
 import layout from '../templates/components/uxs-nav-item';
+import Clickable from 'ember-ux-sauce/mixins/clickable';
 import Testable from 'ember-ux-sauce/mixins/testable';
 import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
 
-const NavItem = Component.extend(BEMComponent, Testable, {
+const NavItem = Component.extend(BEMComponent, Clickable, Testable, {
   // Attributes
   base: 'uxs-nav__item',
   layout,
