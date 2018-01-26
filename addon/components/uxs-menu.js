253dbdf305d352c9a5de652bfffab47b559fbd40
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-01-26 09:05:32 +0000

    Feat (menu) Added menu component

--- addon/components/uxs-menu.js
+++ addon/components/uxs-menu.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import layout from '../templates/components/uxs-nav';
+import layout from '../templates/components/uxs-menu';
 import Testable from 'ember-ux-sauce/mixins/testable';
 import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
 
--- addon/components/uxs-nav-item.js
+++ addon/components/uxs-nav-item.js
@@ -3,11 +3,35 @@ import layout from '../templates/components/uxs-nav-item';
 import Clickable from 'ember-ux-sauce/mixins/clickable';
 import Testable from 'ember-ux-sauce/mixins/testable';
 import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
+import {
+  computed
+} from '@ember/object';
+import {
+  get
+} from '@ember/object';
 
 const NavItem = Component.extend(BEMComponent, Clickable, Testable, {
   // Attributes
   base: 'uxs-nav__item',
   layout,
+  // Methods
+  init() {
+    this._super(...arguments);
+    this.registerModifiers(['disabled']);
+  },
+  // Computed
+  showRightIcon: computed('hasRightIcon', 'rightIcon', 'hasSecondaryText', 'secondaryText', function() {
+    Ember.Logger.log({
+      hasRightIcon: get(this, 'hasRightIcon'),
+      rightIcon: get(this, 'rightIcon'),
+      hasSecondaryText: get(this, 'hasSecondaryText'),
+      secondaryText: get(this, 'secondaryText')
+    });
+    return get(this, 'hasRightIcon') || get(this, 'rightIcon') || get(this, 'hasSecondaryText') || get(this, 'secondaryText');
+  }),
+  showLeftIcon: computed('hasLeftIcon', 'leftIcon', function() {
+    return get(this, 'hasLeftIcon') || get(this, 'leftIcon');
+  }),
 });
 
 NavItem.reopenClass({
