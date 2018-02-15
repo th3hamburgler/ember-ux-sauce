f131a0d19a697a32f411a2979698f27c12261f71
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-02-15 17:06:34 +0000

    Feat (avatar) Added avatar-text

---
+++ addon/components/uxs-avatar-text.js
@@ -0,0 +1,41 @@
+import Component from '@ember/component';
+import layout from '../templates/components/uxs-avatar-text';
+import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
+import Testable from 'ember-ux-sauce/mixins/testable';
+import {
+  equal,
+} from '@ember/object/computed';
+
+const TextAvatar = Component.extend(BEMComponent, Testable, {
+  base: 'uxs-text-avatar',
+  layout,
+  // Computed
+  small: equal('size', 'small'),
+  medium: equal('size', 'medium'),
+  large: equal('size', 'large'),
+  huge: equal('size', 'huge'),
+  giant: equal('size', 'giant'),
+  // Methods
+  init() {
+    this._super(...arguments);
+    this.registerModifiers([
+      'border',
+      'small',
+      'large',
+      'huge',
+      'giant',
+      'round',
+    ]);
+  },
+  getDefaultProps() {
+    return {
+      size: 'medium',
+    };
+  },
+});
+
+TextAvatar.reopenClass({
+  positionalParams: ['text'],
+});
+
+export default TextAvatar;
--- tests/dummy/app/controllers/docs/components/lists.js
+++ tests/dummy/app/controllers/docs/components/lists.js
@@ -9,16 +9,19 @@ export default Controller.extend({
         title: "Item One",
         location: "United Kingdom",
         email: "foo@example.com",
+        count: 7,
       },
       {
         title: "Item Two",
         location: "United Kingdom",
         email: "bar@example.com",
+        count: 99,
       },
       {
         title: "Item Three",
         location: "France",
         email: "boop@example.com",
+        count: 36,
       },
     ]
   }),
