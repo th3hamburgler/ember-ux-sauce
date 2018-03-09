87a72210f7f6d1e047ac005de835422afa5ec20a
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-03-09 12:05:26 +0000

    Feat (div) Make div stylable

--- addon/components/uxs-div.js
+++ addon/components/uxs-div.js
@@ -2,9 +2,35 @@ import Component from '@ember/component';
 import layout from '../templates/components/uxs-div';
 import Clickable from 'ember-ux-sauce/mixins/clickable';
 import Testable from 'ember-ux-sauce/mixins/testable';
+import Styleable from 'ember-ux-sauce/mixins/styleable';
 import BEMComponent from 'ember-bem-sauce/mixins/bem-component';
+import {
+  equal
+} from '@ember/object/computed';
 
-export default Component.extend(BEMComponent, Clickable, Testable, {
+export default Component.extend(BEMComponent, Clickable, Styleable, Testable, {
   base: 'uxs-div',
   layout,
+  // Computed
+  small: equal('size', 'small'),
+  medium: equal('size', 'medium'),
+  large: equal('size', 'large'),
+  huge: equal('size', 'huge'),
+  giant: equal('size', 'giant'),
+  // Methods
+  init() {
+    this._super(...arguments);
+    this.registerModifiers([
+      'small',
+      'medium',
+      'large',
+      'huge',
+      'giant',
+    ]);
+  },
+  getDefaultProps() {
+    return {
+      size: 'medium',
+    };
+  },
 });
