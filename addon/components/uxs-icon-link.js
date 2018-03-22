74dd5b76fe5b896466f5bf89b1a13484ccf664a4
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-03-22 15:22:55 +0000

    Feat (icon) Added icon link component

--- addon/components/uxs-icon-link.js
+++ addon/components/uxs-icon-link.js
@@ -1,6 +1,43 @@
-import Component from '@ember/component';
+import LinkComponent from '@ember/routing/link-component';
 import layout from '../templates/components/uxs-icon-link';
+import Buttonable from 'ember-ux-sauce/mixins/buttonable';
+import Clickable from 'ember-ux-sauce/mixins/clickable';
+import Styleable from 'ember-ux-sauce/mixins/styleable';
+import {
+  alias
+} from '@ember/object/computed';
+import {
+  set
+} from '@ember/object';
+import {
+  PropTypes
+} from 'ember-prop-types';
 
-export default Component.extend({
-  layout
+export default LinkComponent.extend(Buttonable, Clickable, Styleable, {
+  // Attributes
+  base: 'uxs-icon',
+  layout,
+  // Computed
+  icon: alias('linkTitle'),
+  // Methods
+  init() {
+    this._super(...arguments);
+    this.registerModifiers([
+      'accent',
+      'error',
+      'grey',
+      'primary',
+      'prefix',
+      'suffix',
+      'warning',
+    ])
+    set(this, 'propTypes', {
+      icon: PropTypes.string,
+    });
+  },
+  getDefaultProps() {
+    return {
+      style: null,
+    };
+  },
 });
