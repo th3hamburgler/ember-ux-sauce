3d4743b7456f09a8c3144844705a5426b9297ac0
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-10-30 12:38:39 +0000

    Docs (lists) Updated docs for list type components

--- addon/components/uxs-dl.js
+++ addon/components/uxs-dl.js
@@ -2,8 +2,97 @@ import Component from '@ember/component';
 import layout from '../templates/components/uxs-dl';
 import Textable from '../mixins/textable';
 
+/**
+  A typographic component to render definition lists
+
+  ```hbs
+  {{#uxs-dl as |list|}}
+    {{list.term "Coffee"}}
+    {{list.description "Black hot drink"}}
+    {{list.term "Milk"}}
+    {{list.description "White cold drink"}}
+  {{/uxs-dl}}
+  ```
+
+  @class UXSDL
+  @public
+  @yield {Hash} list
+  @yield {Component} list.term
+  @yield {Component} list.description
+*/
 export default Component.extend(Textable, {
+  //
+  // Properties
+  // ---------
+  /**
+    The BEM base name for this component: "uxs-text"
+
+    @property base
+    @type     String
+    @default  "uxs-text"
+    @public
+   */
   base: 'uxs-dl',
+  //
+  // Private
+  // ---------
+  /**
+    The path to the handlbars template
+
+    @property layout
+    @type     String
+    @private
+   */
   layout,
   tagName: 'dl',
+  //
+  // Arguments
+  // ---------
+
+  /**
+    The text align direction, "left", "center", "right" or "justified". Defaults to no alignment (inheret)
+
+    @argument align
+    @default  null
+    @type     String
+  */
+
+  /**
+    Adds a color modifier to the component. Standard options are "white", "light", "mid", "grey", "dark", "primary", "accent", "success", "warning"  or "error". Defaults to no style (inheret).
+
+    @argument color
+    @type String
+  */
+
+  /**
+  The name of the component. This property also defaults to the value for test selectors.<br/>
+
+  The default is true, which ensures the test selector is added. If set to false the test selector is not rendered.
+
+    @argument name
+    @type     Boolean|String
+  */
+
+  /**
+    Adds a style modifier to the component. Standard options are "white", "light", "mid", "grey", "dark", "primary", "accent", "success", "warning"  or "error". Defaults to no style (inheret).
+
+    @argument style
+    @type String
+  */
+
+  /**
+    Define the size of the component. Standard options are "giant", "huge", "large", "medium", "small", or "tiny". Defaults to no size (inhered)
+
+    @argument size
+    @default null
+    @type String
+  */
+
+  /**
+    Define the weight of the component. Standard options are "bold", "medium" or "light". Defaults to no weight (inhered)
+
+    @argument weight
+    @default null
+    @type String
+  */
 });
---
+++ tests/dummy/app/templates/docs/typography/lists.md
@@ -0,0 +1,26 @@
+# Lists
+
+List typography components
+
+## Ordered Lists
+
+Ordered lists are used to present items in a sequence. Play around with the argiments in the demo. To see a full list check out the [api page](/docs/api/components/uxs-ol).
+
+{{docs/uxs-ol/basic-demo}}
+
+## Unordered Lists
+
+Unordered lists are used to present items in a list. Play around with the argiments in the demo. To see a full list check out the [api page](/docs/api/components/uxs-ul).
+
+{{docs/uxs-ul/basic-demo}}
+
+## Definition Lists
+
+Definition lists are used to present a list of definitions. Play around with the argiments in the demo. To see a full list check out the [api page](/docs/api/components/uxs-dl).
+
+{{docs/uxs-dl/basic-demo}}
+
+{{args-table/align-options}}
+{{args-table/size-options}}
+{{args-table/style-options}}
+{{args-table/weight-options}}
