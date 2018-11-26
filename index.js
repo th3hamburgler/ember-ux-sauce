ab02cf4082aabfb3ddb726dc994f953b3c8c1ae6
Jim Wardlaw <jim.wardlaw@gmail.com>
2018-11-26 15:30:13 +0000

    Fix (adding) move venom sass to uxs.scss

--- index.js
+++ index.js
@@ -13,19 +13,18 @@ module.exports = {
   included: function( /* app */ ) {
     this._super.included.apply(this, arguments);
   },
-  // #TODO: Add back for power select
-  // contentFor: function(type, config) {
-  //   /**
-  //    * Ensure we include the ember wormhole placeholder
-  //    * in the main index.html dom
-  //    **/
-  //   if (type === 'body-footer') {
-  //     var emberBasicDropdown = this.addons.filter(function(addon) {
-  //       return addon.name === 'ember-power-select';
-  //     })[0];
-  //     return emberBasicDropdown.contentFor(type, config);
-  //   } else {
-  //     return '';
-  //   }
-  // },
+  contentFor: function(type, config) {
+    /**
+     * Ensure we include the ember wormhole placeholder
+     * in the main index.html dom
+     **/
+    if (type === 'body-footer') {
+      var emberBasicDropdown = this.addons.filter(function(addon) {
+        return addon.name === 'ember-power-select';
+      })[0];
+      return emberBasicDropdown.contentFor(type, config);
+    } else {
+      return '';
+    }
+  },
 };
