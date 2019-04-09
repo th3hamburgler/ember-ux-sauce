c0f01559b279d95717c40a7e1236c2fc7aa939cd
Jim Wardlaw <jim.wardlaw@gmail.com>
2019-04-09 09:52:24 +0100

    Feat (list) added grow attribute for flex biz

--- addon/components/uxs-list.js
+++ addon/components/uxs-list.js
@@ -10,9 +10,59 @@ import {
   isEmpty
 } from '@ember/utils';
 
+/**
+  A component to render a resource list
+
+  ```hbs
+  {{#uxs-list as |list|}}
+    {{list.item ...}}
+  {{/uxs-list}}
+  ```
+  @class UXS List
+  @public
+*/
 export default Component.extend(BEMComponent, Testable, {
-  base: 'uxs-list',
+  // Attributes
   layout,
+  /**
+    The base css class name 'uxs-list'
+
+    @field base
+    @type String
+   */
+  base: 'uxs-list',
+  // Arguments
+  /**
+    Set to true if this lists items should include a border
+
+    @argument bordered
+    @type     Boolean
+    @default  false
+    @public
+   */
+  bordered: false,
+  /**
+    Set to true if this list should grow to fit the flex box parent
+
+    @argument grow
+    @type     Boolean
+    @default  false
+    @public
+   */
+  grow: false,
+  /**
+    Set the style of the splash component.
+
+    UXS ships with the following stock styles: primary, accent, warning, error, dark, grey, mid, light & white.
+
+    You can customise your component by using any string here and adding your own css for the custom modifier e.g. _.uxs-splash--my-custom-style_
+
+    @argument style
+    @type     String
+    @default  null
+    @public
+   */
+  style: null,
   // Computed
   parentStyle: computed('style', function() {
     const style = get(this, 'style');
@@ -25,17 +75,8 @@ export default Component.extend(BEMComponent, Testable, {
     this._super(...arguments);
     this.registerModifiers([
       'bordered',
-      '*style'
+      '*style',
+      'grow',
     ]);
-  },
-  /**
-   * Init any property defaults
-   */
-  getDefaultProps() {
-    return {
-      style: 'white',
-      bordered: false,
-      lines: 1,
-    };
   },
 });
