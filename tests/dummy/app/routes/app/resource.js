b7c976978d740e9a68fe1aa9169df40866a6486f
Jim Wardlaw <jim.wardlaw@gmail.com>
2020-02-04 15:20:44 +0000

    Fix (form) fix datapicker styles

--- tests/dummy/app/routes/app/resource.js
+++ tests/dummy/app/routes/app/resource.js
@@ -9,175 +9,200 @@ export default Route.extend({
       "last_name": "Manuaud",
       "email": "smanuaud0@twitpic.com",
       "gender": "Female",
-      "ip_address": "227.104.109.77"
+      "ip_address": "227.104.109.77",
+      "dob": new Date(),
     }, {
       "id": 2,
       "first_name": "Wilbur",
       "last_name": "Heinish",
       "email": "wheinish1@virginia.edu",
       "gender": "Male",
-      "ip_address": "217.72.63.196"
+      "ip_address": "217.72.63.196",
+      "dob": new Date(),
     }, {
       "id": 3,
       "first_name": "Lalo",
       "last_name": "Bengall",
       "email": "lbengall2@google.com.au",
       "gender": "Male",
-      "ip_address": "143.175.71.172"
+      "ip_address": "143.175.71.172",
+      "dob": new Date(),
     }, {
       "id": 4,
       "first_name": "Leoline",
       "last_name": "Loos",
       "email": "lloos3@artisteer.com",
       "gender": "Female",
-      "ip_address": "48.190.241.91"
+      "ip_address": "48.190.241.91",
+      "dob": new Date(),
     }, {
       "id": 5,
       "first_name": "Sharl",
       "last_name": "Schruyer",
       "email": "sschruyer4@blogs.com",
       "gender": "Female",
-      "ip_address": "37.85.128.30"
+      "ip_address": "37.85.128.30",
+      "dob": new Date(),
     }, {
       "id": 6,
       "first_name": "Deborah",
       "last_name": "Cardenoso",
       "email": "dcardenoso5@ifeng.com",
       "gender": "Female",
-      "ip_address": "7.121.73.127"
+      "ip_address": "7.121.73.127",
+      "dob": new Date(),
     }, {
       "id": 7,
       "first_name": "Garrard",
       "last_name": "Youthead",
       "email": "gyouthead6@digg.com",
       "gender": "Male",
-      "ip_address": "86.129.14.236"
+      "ip_address": "86.129.14.236",
+      "dob": new Date(),
     }, {
       "id": 8,
       "first_name": "Kaleb",
       "last_name": "Melluish",
       "email": "kmelluish7@live.com",
       "gender": "Male",
-      "ip_address": "132.215.6.192"
+      "ip_address": "132.215.6.192",
+      "dob": new Date(),
     }, {
       "id": 9,
       "first_name": "Lyndsey",
       "last_name": "Haggerstone",
       "email": "lhaggerstone8@hc360.com",
       "gender": "Female",
-      "ip_address": "161.233.134.208"
+      "ip_address": "161.233.134.208",
+      "dob": new Date(),
     }, {
       "id": 10,
       "first_name": "Mathilda",
       "last_name": "Abreheart",
       "email": "mabreheart9@cbsnews.com",
       "gender": "Female",
-      "ip_address": "57.171.248.87"
+      "ip_address": "57.171.248.87",
+      "dob": new Date(),
     }, {
       "id": 11,
       "first_name": "Coreen",
       "last_name": "Summerly",
       "email": "csummerlya@bloglovin.com",
       "gender": "Female",
-      "ip_address": "113.158.192.167"
+      "ip_address": "113.158.192.167",
+      "dob": new Date(),
     }, {
       "id": 12,
       "first_name": "Pietrek",
       "last_name": "Royston",
       "email": "proystonb@upenn.edu",
       "gender": "Male",
-      "ip_address": "223.153.89.184"
+      "ip_address": "223.153.89.184",
+      "dob": new Date(),
     }, {
       "id": 13,
       "first_name": "Marlee",
       "last_name": "Duquesnay",
       "email": "mduquesnayc@squarespace.com",
       "gender": "Female",
-      "ip_address": "48.172.27.118"
+      "ip_address": "48.172.27.118",
+      "dob": new Date(),
     }, {
       "id": 14,
       "first_name": "Katerina",
       "last_name": "Fluger",
       "email": "kflugerd@nature.com",
       "gender": "Female",
-      "ip_address": "160.172.144.115"
+      "ip_address": "160.172.144.115",
+      "dob": new Date(),
     }, {
       "id": 15,
       "first_name": "Saba",
       "last_name": "Attrey",
       "email": "sattreye@shutterfly.com",
       "gender": "Female",
-      "ip_address": "15.210.25.49"
+      "ip_address": "15.210.25.49",
+      "dob": new Date(),
     }, {
       "id": 16,
       "first_name": "Alfie",
       "last_name": "Burbridge",
       "email": "aburbridgef@prlog.org",
       "gender": "Female",
-      "ip_address": "133.58.220.206"
+      "ip_address": "133.58.220.206",
+      "dob": new Date(),
     }, {
       "id": 17,
       "first_name": "Federico",
       "last_name": "Staries",
       "email": "fstariesg@vkontakte.ru",
       "gender": "Male",
-      "ip_address": "166.106.149.51"
+      "ip_address": "166.106.149.51",
+      "dob": new Date(),
     }, {
       "id": 18,
       "first_name": "Dotti",
       "last_name": "Deners",
       "email": "ddenersh@quantcast.com",
       "gender": "Female",
-      "ip_address": "19.182.159.110"
+      "ip_address": "19.182.159.110",
+      "dob": new Date(),
     }, {
       "id": 19,
       "first_name": "Belita",
       "last_name": "Jaggar",
       "email": "bjaggari@japanpost.jp",
       "gender": "Female",
-      "ip_address": "148.108.164.227"
+      "ip_address": "148.108.164.227",
+      "dob": new Date(),
     }, {
       "id": 20,
       "first_name": "Umberto",
       "last_name": "Bramble",
       "email": "ubramblej@independent.co.uk",
       "gender": "Male",
-      "ip_address": "45.124.39.153"
+      "ip_address": "45.124.39.153",
+      "dob": new Date(),
     }, {
       "id": 21,
       "first_name": "Onida",
       "last_name": "Hebson",
       "email": "ohebsonk@shareasale.com",
       "gender": "Female",
-      "ip_address": "95.117.233.199"
+      "ip_address": "95.117.233.199",
+      "dob": new Date(),
     }, {
       "id": 22,
       "first_name": "Obediah",
       "last_name": "Delagua",
       "email": "odelagual@washington.edu",
       "gender": "Male",
-      "ip_address": "0.126.211.46"
+      "ip_address": "0.126.211.46",
+      "dob": new Date(),
     }, {
       "id": 23,
       "first_name": "Milly",
       "last_name": "Kiellor",
       "email": "mkiellorm@1688.com",
       "gender": "Female",
-      "ip_address": "68.198.4.57"
+      "ip_address": "68.198.4.57",
+      "dob": new Date(),
     }, {
       "id": 24,
       "first_name": "Ilene",
       "last_name": "Aldwich",
       "email": "ialdwichn@multiply.com",
       "gender": "Female",
-      "ip_address": "204.23.203.0"
+      "ip_address": "204.23.203.0",
+      "dob": new Date(),
     }, {
       "id": 25,
       "first_name": "Bondie",
       "last_name": "Dockray",
       "email": "bdockrayo@ucoz.com",
       "gender": "Male",
-      "ip_address": "61.60.44.86"
+      "ip_address": "61.60.44.86",
+      "dob": new Date(),
     }];
     return people[params.id];
   }
